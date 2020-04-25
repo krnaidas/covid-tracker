85fe5f614c28d53beba87f47f2c9e86a1242292e
Karlos Naidas <krnaidas@gmail.com>
2020-04-25 16:28:44 +0800

    First Commit

---
+++ Covid-Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Covid-Tracker
+//
+//  Created by Naidas on 4/25/20.
+//  Copyright © 2020 Naidas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
