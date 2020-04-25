6ce4a6a3c633b4c548f4f084ef53bb7655684513
Karlos Naidas <krnaidas@gmail.com>
2020-04-25 21:29:23 +0800

    initial VC setup

--- Covid-Tracker/Controllers/TrackerViewController.swift
+++ Covid-Tracker/Controllers/TrackerViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class TrackerViewController: UIViewController {
-
+    
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    @IBOutlet weak var countryLabel: UILabel!
+    
+    @IBOutlet weak var currentCasesLabel: UILabel!
+    @IBOutlet weak var currentDeathsLabel: UILabel!
+    
+    @IBOutlet weak var newCasesLabel: UILabel!
+    @IBOutlet weak var newRecoveriesLabel: UILabel!
+    @IBOutlet weak var newDeathsLabel: UILabel!
+    @IBOutlet weak var totalTestsLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func searchedPressed(_ sender: Any) {
+        print("searched pressed")
+    }
+    
+    @IBAction func getLocationPressed(_ sender: Any) {
+        print("location pressed")
+    }
+    
+    
 }
 
